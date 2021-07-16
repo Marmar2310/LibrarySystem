398f3945e0ed44a057f09ecb1c1cf9d67d8c8eb0
Omar Lamrani <lamraniomarcs@gmail.com>
2021-07-16 21:41:44 +0100

    Added option to add multiple books at once and to list all available book by a single author

--- Library.py
+++ Library.py
@@ -16,20 +16,35 @@ def add_book(name, author):
     temp_book.author = author
     temp_book.ID = get_next_avail_ID()
     temp_book.taken = False
-
     book_list.append(temp_book)
+    return "added book" + name + "by" + author
+
+def add_list_books(arr_book):
+    for x in arr_book:
+        temp_book = Book()
+        temp_book.name = x[0]
+        temp_book.author = x[1]
+        temp_book.ID = get_next_avail_ID()
+        temp_book.taken = False
+        book_list.append(temp_book)
+        print("added" + x[0] + "by" + x[1])
 
 def take_book(name):
     for x in book_list:
         if x.name == name:
             x.taken = True
-    return 0
+    return "took book" + name
 
 def return_book(name):
     for x in book_list:
         if x.name == name:
             x.taken = False
-    return 0
+    return "returned book" + name
+
+def list_author_books(author):
+    for x in book_list:
+        if x.author == author:
+            print(x.name)
 
 def get_next_avail_ID():
     return len(book_list)
